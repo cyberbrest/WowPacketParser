2d37f2d51033b1d939990c9fd2c5588e585336ba
QAston <qaston@gmail.com>
2012-04-14 22:02:11 +0200

    Bring back support for multiple reader types, add an option to select desired loaded.

--- WowPacketParser/Misc/Settings.cs
+++ WowPacketParser/Misc/Settings.cs
@@ -19,6 +19,7 @@ namespace WowPacketParser.Misc
         public static readonly int FilterPacketNumHigh = GetInt32("FilterPacketNumHigh", 0);
         public static readonly int FilterPacketsNum = GetInt32("FilterPacketsNum", 0);
         public static readonly ClientVersionBuild ClientBuild = GetEnum("ClientBuild", ClientVersionBuild.Zero);
+        public static readonly string PacketFileType = GetString("PacketFileType", string.Empty);
         public static readonly int ThreadsRead = GetInt32("Threads.Read", 0);
         public static readonly int ThreadsParse = GetInt32("Threads.Parse", 0);
         public static readonly DumpFormatType DumpFormat = GetEnum("DumpFormat", DumpFormatType.Text);
